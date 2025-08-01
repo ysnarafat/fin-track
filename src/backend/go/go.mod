db3bf8035aee9e5e586f16aca4202fc51c3bb4ab
S.M. Imran Emon <14341628+IMRAN104@users.noreply.github.com>
2025-08-01 21:02:14 +0600

    Revert "Setup Skeleton for FE and BE"

--- src/backend/go/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/ysnarafat/fin-track
-
-go 1.24.2
-
-require (
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
--- src/backend/go/internal/repository/repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/ysnarafat/fin-track/internal/domain"
-
-type ExpenseRepository interface {
-    Create(exp domain.Expense) error
-    Get() ([]domain.Expense, error)
-}
